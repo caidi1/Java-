703af3bbea6e3968eb21ae333525d1119e03649e
caidi <129133538+caidi1@users.noreply.github.com>
2023-04-04 20:00:14 +0800

    Add files via upload

---
+++ 检测素数.java
@@ -0,0 +1,25 @@
+package xun;
+
+import java.util.Scanner;
+
+public class 检测素数 {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int i;
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                System.out.println(n + "不是素数");
+                break;
+            }
+        }
+            if (n==i){
+                System.out.println(n+"是素数");
+
+            }
+    }
+
+
+
+}
+
